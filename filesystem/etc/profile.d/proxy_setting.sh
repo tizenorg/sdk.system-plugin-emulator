392b504bb6b6d9853e9fe1ec207c16ebce9bbf93
SeokYeon Hwang <syeon.hwang@samsung.com>
2013-03-29 17:01:22 +0900

    Remove sdbd specified jobs from proxy_setting.sh
    It is moved to sdbd.service
    
    Change-Id: I991dbf22e59bf82885eaaaea463d91d863685d00
    Signed-off-by: SeokYeon Hwang <syeon.hwang@samsung.com>

--- filesystem/etc/profile.d/proxy_setting.sh
+++ filesystem/etc/profile.d/proxy_setting.sh
@@ -1,26 +1,17 @@
 #!/bin/sh
 if grep -q "http_proxy=" /proc/cmdline ; then
         __proxy=`sed 's/^.*http_proxy=\([^, ]*\).*$/\1/g' /proc/cmdline`
-        echo -e "[Export environment variable: http_proxy=${__proxy}]"
         export "http_proxy=${__proxy}"
 fi
 if grep -q "https_proxy=" /proc/cmdline ; then
         __proxy=`sed 's/^.*https_proxy=\([^, ]*\).*$/\1/g' /proc/cmdline`
-        echo -e "[Export environment variable: https_proxy=${__proxy}]"
         export "https_proxy=${__proxy}"
 fi
 if grep -q "ftp_proxy=" /proc/cmdline ; then
         __proxy=`sed 's/^.*ftp_proxy=\([^, ]*\).*$/\1/g' /proc/cmdline`
-        echo -e "[Export environment variable: ftp_proxy=${__proxy}]"
         export "ftp_proxy=${__proxy}"
 fi
 if grep -q "socks_proxy=" /proc/cmdline ; then
         __proxy=`sed 's/^.*socks_proxy=\([^, ]*\).*$/\1/g' /proc/cmdline`
-        echo -e "[Export environment variable: socks_proxy=${__proxy}]"
         export "socks_proxy=${__proxy}"
-fi
-if grep -q "sdb_port=" /proc/cmdline ; then
-        __sdb_port=`sed 's/^.*sdb_port=\([^, ]*\).*$/\1/g' /proc/cmdline` 
-    	rm -rf /opt/home/sdb_port.txt
-	echo "${__sdb_port}" >> /opt/home/sdb_port.txt
 fi
